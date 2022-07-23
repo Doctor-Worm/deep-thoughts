a41c7174ebb47354dfa06c23d2bfbeee9cdc4522
Skylar Jackson <sky.jackson@icloud.com>
2022-07-23 12:22:22 -0400

    setup graphql

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,8 +2,28 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
-        thoughts: async () => {
+        // get thought by username. if no username, return all thoughts
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
             return Thought.find().sort({ createdAt: -1 });
+        },
+        // get single thought by _id
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__V -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // get user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
         }
     }
 };
